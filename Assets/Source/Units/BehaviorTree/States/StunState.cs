bcb06affd74143b6760d9a305dc8b05a712fbd4e
Nikita Trenikhin <nik.trenikhin@gmail.com>
2023-01-11 13:00:04 +0500

    add some fixes

--- Assets/Source/Units/BehaviorTree/States/StunState.cs
+++ Assets/Source/Units/BehaviorTree/States/StunState.cs
@@ -7,18 +7,22 @@ using UnityEngine.AI;
 public class StunState : State
 {
     private NavMeshAgent _agent;
-    private float _currentSpeed;
+    private float _speed;
+
+    public float CurrentSpeed => _agent.speed;
+
+    public float NormalSpeed => _speed;
 
     private readonly int _stunTime = 5;
 
     private IEnumerator Start()
     {
         _agent = GetComponent<NavMeshAgent>();
-        _currentSpeed = _agent.speed;
+        _speed = _agent.speed;
         _agent.speed = 0;
 
         yield return new WaitForSeconds(_stunTime);
 
-        _agent.speed = _currentSpeed;
+        _agent.speed = _speed;
     }
 }
