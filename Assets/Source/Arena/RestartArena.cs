e056076407ec91a3b9f56251071c10ea1f3aa35f
Nikita Trenikhin <nik.trenikhin@gmail.com>
2023-03-08 00:52:19 +0500

    Cleaner added

--- Assets/Source/Arena/RestartArena.cs
+++ Assets/Source/Arena/RestartArena.cs
@@ -10,9 +10,11 @@ public class RestartArena : MonoBehaviour
     [SerializeField] private SquadHealthbar _playerHealth;
     [SerializeField] private SquadHealthbar _enemyHealth;
     [SerializeField] private GameObject _buttons;
+    [SerializeField] private BulletCleaner _cleaner;
 
     public void OnRestart()
     {
+        _cleaner.gameObject.SetActive(true);
         _buttons.SetActive(true);
         _playerHealth.UpdateHealthbar();
         _enemyHealth.UpdateHealthbar();
